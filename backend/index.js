7544c23d675cb7354875d1940d6698ded82cfe23
Nikhil Prakash <nikhilprakash628@gmail.com>
2025-07-15 13:39:34 +0530

    This is updated project

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-const corsOptions = {
+const corsOptions = { 
     origin: process.env.frontend_URL,
     credentials:true
 }
